56f48fde494348013b12cd83aae29f6b4b0836cc
Victor Aires <victorairesjp@gmail.com>
2026-04-13 14:13:05 -0300

    Feat: delete button for each link Update: list render without "http"

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const inputEl = document.getElementById("input-el")
 const openAllEl = document.getElementById("openAll-btn")
 const addLinkEl = document.getElementById("addLink-btn")
-const deleteLinkEl = document.getElementById("deleteLink-btn")
 const clearAllEl = document.getElementById("clearAll-btn")
 const ulEl = document.getElementById("ul-el")
 const loadFromLocalStorage = JSON.parse(localStorage.getItem("myLinks"))
@@ -34,15 +33,35 @@ addLinkEl.addEventListener("click", function() {
 function render(list) {
     let listLinks = ""
     for (let i = 0; i < list.length; i++) {
+        let label
+        try {
+            const url = list[i].startsWith("http") ? list[i] : "https://" + list[i]
+            label = new URL(url).hostname.replace(/^www\./, "")
+        } catch (_) {
+            label = list[i]
+        }
         listLinks += `
             <li>
-                <a target='_blank' href='${list[i]}'>
-                    ${list[i]}
+                <a target='_blank' href='${list[i]}' title='${list[i]}'>
+                    ${label}
                 </a>
+                <button class='btn-delete-item' data-index='${i}' title='Remover link'>
+                    &times;
+                </button>
             </li>
         `
     }
     ulEl.innerHTML = listLinks
+
+    // Adiciona evento de clique nos botões de deletar individuais
+    document.querySelectorAll(".btn-delete-item").forEach(function(btn) {
+        btn.addEventListener("click", function() {
+            const index = parseInt(this.getAttribute("data-index"))
+            myLinks.splice(index, 1)
+            localStorage.setItem("myLinks", JSON.stringify(myLinks))
+            render(myLinks)
+        })
+    })
 
     const emptyState = document.getElementById("empty-state")
     if (emptyState) {
@@ -62,11 +81,6 @@ openAllEl.addEventListener("click", function() {
 })
 
 
-// Função para deletar um link da lista (myLinks)
-deleteLinkEl.addEventListener("dblclick", function() {
-    myLinks.pop()
-    render(myLinks)
-})
 
 
 // Função para deletar todos os links da lista (myLinks)
